f6b88e2044a21dc1c925b4fdc49dbc759887e7e4
mi201334 <mi201334@edu.okinawa-ct.ac.jp>
2023-11-01 16:09:11 +0900

    DBConnectionを設定

--- backend/main.go
+++ backend/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"main/router"
+	"main/model"
 )
 
 func main() {
+	sqlDB := model.DBConnection()
+	defer sqlDB.Close()
   // インスタンスを作成
   e := echo.New()
 	router.SetRouter(e)
