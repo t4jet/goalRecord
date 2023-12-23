2704a0b896f4a5974183826087b0c4ab22d9abce
mi201334 <mi201334@edu.okinawa-ct.ac.jp>
2023-12-23 20:14:52 +0900

    dashboard page

---
+++ frontend/src/pages/dashboard.jsx
@@ -0,0 +1,13 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>全てのタスク</h1>
+      <ul>
+        <li>タスク1</li>
+        <li>タスク2</li>
+        <li>タスク3</li>
+      </ul>
+    </div>
+  )
+}
+
