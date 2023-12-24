d199728c864105d510f4ae237e928a8c32757dde
mi201334 <mi201334@edu.okinawa-ct.ac.jp>
2023-12-24 13:27:20 +0900

    fetch data

--- frontend/src/app/dashboard/page.jsx
+++ frontend/src/app/dashboard/page.jsx
@@ -1,32 +1,35 @@
 'use client'
-
 import { useEffect, useState } from "react";
 
 export default function Dashboard() {
-  const [tasks, setTasks] = useState([]);
+const [tasks, setTasks] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`/api/dashboard/`, {
-          method: "GET",
-        });
-        const responseJSON = await response.json();
-        const setTasks = responseJSON.data;
-      } catch (error) {
-        console.error(error)
-      }
-      fetchData();
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`/api/dashboard`, {
+        method: "GET",
+      });
+      const responseJSON = await response.json();
+      const fetchedTasks = responseJSON.data;
+      setTasks(fetchedTasks);
+    } catch (error) {
+      console.error(error);
     }
-  }, [])
+  };
+
+  fetchData();
+}, []);
+
   return (
     <div>
       <ul>
-        {tasks.map((task) => (
-        <li key={task.id}>{task.name}</li>
-        ))}
+        {tasks.length && (
+          tasks.map((task,id) => (
+            <li key={id}>{task.name}</li>
+          ))
+        )}
       </ul>
     </div>
-  )
+  );
 }
-
