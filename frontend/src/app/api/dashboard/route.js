91297445d7a801bf37892ce6cfe838bf6d6a94d3
mi201334 <mi201334@edu.okinawa-ct.ac.jp>
2023-12-24 13:27:32 +0900

    [update] json data

--- frontend/src/app/api/dashboard/route.js
+++ frontend/src/app/api/dashboard/route.js
@@ -1,10 +1,18 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
-  const task = {
-    id:1,
+  const task = [{
+    id: 1,
     name: "掃除",
-  };
+  },
+  {
+    id:2,
+    name: "お風呂",
+  },
+  {
+    id:3,
+    name: "課題",
+  }]  
   return NextResponse.json(
     { data: task, status: 200 }
   );
