a8d0076bdec70b4f969b6224b56cd1c30be85de5
mi201334 <mi201334@edu.okinawa-ct.ac.jp>
2023-12-24 14:23:34 +0900

    [style] update

--- frontend/src/app/dashboard/page.jsx
+++ frontend/src/app/dashboard/page.jsx
@@ -1,32 +1,35 @@
 'use client'
 import { useEffect, useState } from "react";
+import style from '../styles/app.module.scss'
+import TodoItem from "../components/TodoItem";
 
 export default function Dashboard() {
-const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const response = await fetch(`/api/dashboard`, {
-        method: "GET",
-      });
-      const responseJSON = await response.json();
-      const fetchedTasks = responseJSON.data;
-      setTasks(fetchedTasks);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`/api/dashboard`, {
+          method: "GET",
+        });
+        const responseJSON = await response.json();
+        const fetchedTasks = responseJSON.data;
+        setTasks(fetchedTasks);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-  fetchData();
-}, []);
+    fetchData();
+  }, []);
 
   return (
-    <div>
+    <div className={style.content__wrapper}>
       <ul>
         {tasks.length && (
-          tasks.map((task,id) => (
-            <li key={id}>{task.name}</li>
+          tasks.map((task) => (
+            // <li key={task.id}>{task.name}</li>
+            <TodoItem key={task.id} todo={task.name} />
           ))
         )}
       </ul>
